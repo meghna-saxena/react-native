ee71f7aa6c4d5ad113bfffb3b23af3779d15e551
Meghna Srivastava <meghna.s1906@gmail.com>
2018-06-02 17:17:22 +0200

    understanding textinput component

--- TestProject/App.js
+++ TestProject/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, Image, View } from 'react-native';
-import Greeting from './src/Greeting';
-import Blink from './src/Blink';
-import Style from './src/Style';
+// import Greeting from './src/Greeting';
+// import Blink from './src/Blink';
+// import Style from './src/Style';
 // import Flex from './src/Flex';
-import Layout from './src/Layout';
+// import Layout from './src/Layout';
+import TextInput from './src/TextInput';
 
 export default class App extends React.Component {
   render() {
@@ -12,7 +13,7 @@ export default class App extends React.Component {
       uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
     };
     return (
-      <Layout />
+      <TextInput />
       // <View style={styles.container}>
       //   <Text>Test!</Text>
       //   <Image source={pic} style={{ width: 193, height: 110 }} />
--- TestProject/src/Layout.js
+++ TestProject/src/Layout.js
@@ -5,10 +5,15 @@ export default class FlexDirectionBasics extends Component {
     render() {
         return (
             // Try setting `flexDirection` to `column`.
-            <View style={{ flex: 1, flexDirection: 'row' }}>
-                <View style={{ flex: 1, backgroundColor: 'powderblue' }} />
-                <View style={{ flex: 1, backgroundColor: 'skyblue' }} />
-                <View style={{ flex: 1, backgroundColor: 'steelblue' }} />
+            <View style={{
+                flex: 1,
+                flexDirection: 'column',
+                justifyContent: 'center',
+                alignItems: 'center',
+            }}>
+                <View style={{ width: 50, height: 50, backgroundColor: 'powderblue' }} />
+                <View style={{ width: 50, height: 50, backgroundColor: 'skyblue' }} />
+                <View style={{ width: 50, height: 50, backgroundColor: 'steelblue' }} />
             </View>
         );
     }
